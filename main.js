6c7c83b91f8aa05e2e8bc7264d55192140a280eb
Walter Fritsche <wfritsche95@protonmail.com>
2020-09-10 16:39:19 -0500

    fix grid to remove all cells during reset then add new specified cells

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
-function createGrid() {
+function createGrid(num = 16) {
   let gridDisplay = document.querySelector("#grid");
   let gridDisplayStyle = document.querySelector("#grid").style;
   gridDisplayStyle.setProperty("display", "grid");
-  gridDisplayStyle.setProperty("grid-template-columns", "repeat(16, 1fr)");
-  gridDisplayStyle.setProperty("grid-template-rows", "repeat(16, 1fr)");
+  gridDisplayStyle.setProperty("grid-template-columns", `repeat(${num}, 1fr)`);
+  gridDisplayStyle.setProperty("grid-template-rows", `repeat(${num}, 1fr)`);
   // gridDisplayStyle.setProperty("column-gap", "5px");
   // gridDisplayStyle.setProperty("row-gap", "5px");
 
-  // 256 refers to the square of 16 cells
-  for (let i = 0; i < 256; i++) {
+  // Math.pow(num, 2) refers to the square of num cells
+  // so 16x16 will give 256 cells
+  for (let i = 0; i < Math.pow(num, 2); i++) {
     let gridDiv = document.createElement("div");
     gridDiv.classList.add("cell");
     // gridDiv.textContent = "a";
     gridDisplay.appendChild(gridDiv);
   }
+  const gridCells = document.querySelectorAll(".cell");
+  console.log(gridCells);
+  gridCells.forEach(cell => {
+    cell.addEventListener('mouseover', () => {
+      turnCellBlack(cell);
+    });
+  });
 }
 
 function turnCellBlack(cell) {
@@ -21,24 +29,30 @@ function turnCellBlack(cell) {
 }
 
 function resetCells() {
-  const cells = document.querySelectorAll(".cell");
-  // cells.removeAttribute("style");
+  // const cells = document.querySelectorAll(".cell");
+  // // cells.removeAttribute("style");
+  // cells.forEach(cell => {
+  //   cell.removeAttribute("style");
+  // });
+  let cells = document.querySelectorAll(".cell");
+  let gridDisplay = document.querySelector("#grid");
   cells.forEach(cell => {
-    cell.removeAttribute("style");
-  })
+    gridDisplay.removeChild(cell);
+  });
+}
+
+function askForNewGrid() {
+  let reply = parseInt(prompt("How many squares per side?"));
+  console.log(reply);
+  createGrid(reply);
 }
 
 createGrid();
+// Below this line was the original placement of the gridCells event listener;
 
-const gridCells = document.querySelectorAll(".cell");
-console.log(gridCells);
-gridCells.forEach(cell => {
-  cell.addEventListener('mouseover', () => {
-    turnCellBlack(cell);
-  });
-});
 
 const resetButton = document.querySelector("#reset-btn");
 resetButton.addEventListener("click", () => {
   resetCells();
+  askForNewGrid();
 });
